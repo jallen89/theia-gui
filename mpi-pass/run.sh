8dc06177c5138b9dcaab62bb7d86c1dc092e1120
Joey Allen <jallen309@gatech.edu>
2018-12-28 14:55:44 -0500

    Add updated run.sh

--- mpi-pass/run.sh
+++ mpi-pass/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-pass="/home/joey/markdowns/Theia/gui-notes/mpi/llvm-pass-skeleton/build/skeleton/libSkeletonPass.so"
+pass="/home/joey/mProv/ui-work/mpi-pass/build/partition/libPartitionPass.so"
 
 clang-4.0 -c -emit-llvm $1
-opt-4.0  -load $pass -SkeletonPass < ${1%.c}.bc > test.bc
+opt-4.0  -load $pass -PartitionPass < ${1%.c}.bc > tmp.bc
+llc-4.0 -filetype=obj tmp.bc
+gcc -c hello.c -o hello.o
+gcc /home/joey/mProv/ui-work/context-lib/src/*.o -I/home/joey/mProv/ui-work/context-lib/include tmp.o -o ${1%.c}-inst -lpthread
